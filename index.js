671ff3652bd7e1d25258df8f94110cc7d512827c
Ramakrishna Atluri <ramakrishna.atluri@realpage.com>
2020-04-25 19:07:37 +0530

    adding server side success callback

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser')
+const multer = require('multer') // v1.0.5
+const upload = multer() // for parsing multipart/form-data
+const superagent = require('superagent');
+
 
 //setting middleware
 app.use(express.static('public')); //Serves resources from public folder
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+//ROUTES
+//Submit of the form
+app.post('/submitForm', function (req, res) {
+   let captchaToken = req.body['g-recaptcha-response'];
+   let captchaVerifyURL = 'https://www.google.com/recaptcha/api/siteverify';
+   let captchaServerKey = '6Le88e0UAAAAAFjNaQEnqFhfaiRfx-IGCqQ1UdBg';
+
+superagent.post(captchaVerifyURL)
+        .send({ secret: captchaServerKey, response: captchaToken })
+        .then( ()=> res.redirect('/home'))
+        .catch(()=> res.redirect('/error'));
+  });
 
 var server = app.listen(process.env.PORT);
